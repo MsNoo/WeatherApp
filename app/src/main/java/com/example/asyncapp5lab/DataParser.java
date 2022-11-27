4c23229368f20dc33a112af12772c949e6444276
MsNoo <renata.marcinauskaite@gmail.com>
2022-11-27 10:50:23 +0200

    Arr working

--- app/src/main/java/com/example/asyncapp5lab/DataParser.java
+++ app/src/main/java/com/example/asyncapp5lab/DataParser.java
@@ -1,5 +1,7 @@
 package com.example.asyncapp5lab;
 
+import android.os.Build;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -7,6 +9,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DataParser {
     public static String getVilniusWeather(InputStream stream) throws IOException{
@@ -19,24 +24,30 @@ public class DataParser {
         }
 
         String result = "";
-        String conditionCodeStr ="";
+
 
         try{
             JSONObject jData = new JSONObject(data);
             JSONObject placeNode = jData.getJSONObject("place");
             String nameNode = placeNode.getString("name");
+            String airTemperatureStr = "";
+            String conditionCodeStr = "";
+            Integer airTemperature = null;
 
             //forecastTimestamps Arr
-            JSONArray forecastTimestampsArr = placeNode.getJSONArray("forecastTimestamps");
+            JSONArray forecastTimestampsArr = jData.getJSONArray("forecastTimestamps");
             for(int i = 0; i < forecastTimestampsArr.length(); i++){
                 JSONObject innerObject = forecastTimestampsArr.getJSONObject(i);
                 conditionCodeStr = innerObject.getString("conditionCode");
+                airTemperature = innerObject.getInt("airTemperature");
+                airTemperatureStr = String.valueOf(airTemperature);
             }
-            result = String.format("Place: %s\n, Temperature: %s",nameNode, conditionCodeStr);
+            result = String.format("Place: %s\nCondition: %s\nTemperature: %s", nameNode, conditionCodeStr,airTemperatureStr);
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return result;
     }
+
 }
